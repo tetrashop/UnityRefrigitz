2b7423a9fc1f14b396aebb75f542c7ac464b140c
tetrashop <ramin.edjlal1359@gmail.com>
2020-04-18 09:03:27 +0430

    Begin Board manager;
    Codes are ready;neads to manages them;

---
+++ Assets/ManagerBoard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerBoard : MonoBehaviour {
+	RefrigtzChessPortable.RefrigtzChessPortableForm t=null;
+	// Use this for initialization
+	void Start () {
+		t = new RefrigtzChessPortable.RefrigtzChessPortableForm ();
+		t.Form1_Load ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
